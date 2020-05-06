89b9e803af4dc37a6c1e5410320f9ed9339eada8
anuva04 <anuvacotton2016@gmail.com>
2020-05-06 14:44:57 +0530

    added /send route

--- countries.js
+++ countries.js
@@ -4,35 +4,41 @@ const geocode=require('./utils/geocode')
 const forecast=require('./utils/forecast')
 
 const connectURL='mongodb://127.0.0.1:27017'
-const dbname='task-manager-api'
+const dbname='safar-api'
 
 MongoClient.connect(connectURL,{useNewUrlParser:true},(error,client)=>{
   if(error){
       return console.log('error')
   }
   const db=client.db(dbname)
-  db.collection('locations').findOne({place:'paris'},(error,loc)=>{
+  db.collection('locations').insertOne({place: 'guwahati'}, (error, loc) => {
     if(error){
       return console.log(error)
     }
     console.log(loc.place)
-    //converting place name to coordinates using geocode
+  })
+  db.collection('locations').findOne({place:'guwahati'},(error,loc)=>{
+    if(error){
+      return console.log(error)
+    }
+    console.log(loc.place)
+    
     geocode(loc.place,(error,{location,latitude,longitude}={})=>{ 
       if(error){
           return console.log(error)
       }
-      //fetching forecastdata
+      
       forecast(latitude,longitude,(error,forecastData)=>{
           if(error){
               return console.log(error)
            }
-          //writing details into the database
+          
           const entry=new destination({
             place:loc.place,
             precipitation:forecastData.precipitation,
             temperature:forecastData.temp
           })
-          //saving the work in the DB
+          
           entry.save().then(()=>{
             console.log('Destination added succesfully')
           }).catch((error)=>{
---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const {MongoClient,ObjectID}=require('mongodb')
+const destination=require('./model')
+const geocode=require('./utils/geocode')
+const forecast=require('./utils/forecast')
+
+const connectURL='mongodb://127.0.0.1:27017'
+const dbname='safar-api'
+
+const app = express()
+
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.post('/send', (req, res) => {
+    MongoClient.connect(connectURL,{useNewUrlParser:true},(error,client)=>{
+        if(error){
+            return console.log('error')
+        }
+        const db=client.db(dbname)
+        // db.collection('locations').insertOne({place: req.body.place}, (error, loc) => {
+        //   if(error){
+        //     return console.log(error)
+        //   }
+        //   console.log(loc.place)
+        // })
+        // res.render('index', {success: 'Record Inserted Successfully'})
+        db.collection('locations').findOne({place:req.body.place},(error,loc)=>{
+          if(error){
+            return console.log(error)
+          }
+          console.log(loc.place)
+          
+          geocode(loc.place,(error,{location,latitude,longitude}={})=>{ 
+            if(error){
+                return console.log(error)
+            }
+            
+            forecast(latitude,longitude,(error,forecastData)=>{
+                if(error){
+                    return console.log(error)
+                 }
+                
+                const entry=new destination({
+                  place:loc.place,
+                  precipitation:forecastData.precipitation,
+                  temperature:forecastData.temp
+                })
+                
+                entry.save().then(()=>{
+                  console.log('Destination added succesfully')
+                }).catch((error)=>{
+                  console.log(error)
+                })
+            })
+          })
+        })
+        res.render('index', {success: 'Record Inserted Successfully'})
+      })
+})
+
+app.get('/', (req, res) => {
+    res.render('index', { layout:false, success: ''});
+  });
+
+  app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
