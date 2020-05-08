ead0215eafa07fd0e9f44ba9111c7b0c8542ef0c
anuva04 <anuvacotton2016@gmail.com>
2020-05-08 21:56:46 +0530

    added routes for buttons

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(publicDir))
 
-app.post('/', (req, res) => {
+app.post('/update', (req, res) => {
     // MongoClient.connect(connectURL,{useNewUrlParser:true},(error,client)=>{
     //     if(error){
     //         return console.log('error')
@@ -62,14 +62,89 @@ app.post('/', (req, res) => {
           })
         })
      // },7000)
-
-
-    
+    }) 
+})
+app.get('/rainy', (req, res) => {
+  MongoClient.connect(connectURL,{useNewUrlParser:true},(error,client) => {
+    if(error){
+      return console.log('error')
+    }
+    const db=client.db(dbname)
+    var toGo2=[]
+    db.collection('locations').find().forEach((loc)=>{
+      if(loc.precipitation>0){
+          var x =toGo2.push(loc.place)
+      }
+  
+    }).then(()=>{
+      console.log("Rainy places:")
+      if(toGo2.length==0){
+          return console.log("No such destinations")
+      }
+      var iterator = toGo2.values();
+      for (let elements of iterator) { 
+        console.log(elements);
+      }
     })
-    
+    res.render('index', {success: `Record Inserted Successfully`,Places:`Here are the countries`})
+  })
+})
+
+app.get('/warm', (req, res) => {
+  MongoClient.connect(connectURL, {useNewUrlParser:true},(error,client) => {
+    if(error){
+      return console.log('error')
+    }
+    const db=client.db(dbname)
+    var toGo=[]
+    //for warm places
+    db.collection('locations').find().forEach((loc)=>{
+     if(loc.temperature>25){
+         var x =toGo.push(loc.place)
+     }
+ 
+   }).then(()=>{
+     console.log("Warm places:")
+     if(toGo.length==0){
+         return console.log("No such destinations")
+     }
+     var iterator = toGo.values();
+     for (let elements of iterator) { 
+       console.log(elements);
+     }
+   })
+   res.render('index', {success: `Record Inserted Successfully`,Places:`Here are the countries`})
+  })
+})
+
+app.get('/cold', (req, res) => {
+  MongoClient.connect(connectURL, {useNewUrlParser:true},(error,client) => {
+    if(error){
+      return console.log('error')
+    }
+    const db=client.db(dbname)
+    //for cold places
+    var toGo1=[]
+    db.collection('locations').find().forEach((loc)=>{
+      if(loc.temperature<18){
+          var x =toGo1.push(loc.place)
+      }
+
+    }).then(()=>{
+      console.log("Cold places:")
+      if(toGo1.length==0){
+          return console.log("No such destinations")
+      }
+      var iterator = toGo1.values();
+      for (let elements of iterator) { 
+        console.log(elements);
+      }
+    })
+    res.render('index', {success: `Record Inserted Successfully`,Places:`Here are the countries`})
+  })
 })
 app.get('/', (req, res) => {
-    res.render('index', { layout:false,success: '',Places:''});
+    res.render('index', { layout:false,success: ``,Places:``});
   });
 
 app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
