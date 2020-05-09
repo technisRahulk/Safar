5ca000606539fce95168b07807b914cdfdafc9cb
technisRahulk <rahulkumaru99@gmail.com>
2020-05-09 12:38:11 +0530

    data rendered

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 
 const express = require('express')
 const path = require('path')
+const hbs=require('hbs')
 const bodyParser = require('body-parser')
 const {MongoClient,ObjectID}=require('mongodb')
 const destination=require('./utils/model')
@@ -13,138 +14,112 @@ const dbname='safar-api'
 const app = express()
 const publicDir=path.join(__dirname,'./../public')
 app.set('views', path.join(__dirname, './../views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'hbs');
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(publicDir))
 
-app.post('/update', (req, res) => {
-    // MongoClient.connect(connectURL,{useNewUrlParser:true},(error,client)=>{
-    //     if(error){
-    //         return console.log('error')
-    //     }
-    //     const db=client.db(dbname)
-    //     db.collection('locations').insertOne({place:req.body.place}, (error, loc) => {
-    //       if(error){
-    //         return console.log(error)
-    //       }
-    //       console.log(loc.place)
-    //     })
-    //     res.render('index', {success: 'Record Inserted Successfully',Places:'Here are the countries'})
-    //   })
+app.get('/update', (req, res) => {
     MongoClient.connect(connectURL,{useNewUrlParser:true},(error,client)=>{
       if(error){
-          return console.log('error')
+        return res.send('Cannot connect to database')
       }
       const db=client.db(dbname)
-     // setInterval(()=>{
-        db.collection('locations').find().forEach(function(loc){
-          var place_id = loc._id
-          var place_name = loc.place
-          geocode(loc.place,(error,{loc,latitude,longitude}={})=>{ 
+      db.collection('locations').find().forEach(function(loc){
+        var place_id = loc._id
+        var place_name = loc.place
+        geocode(loc.place,(error,{loc,latitude,longitude}={})=>{ 
+          if(error){
+            return res.send({error})
+          }
+          
+          forecast(latitude,longitude,(error,forecastData)=>{
             if(error){
-              return console.log(error)
+              return res.send({error})
             }
-            
-            forecast(latitude,longitude,(error,forecastData)=>{
-              if(error){
-                return console.log(error)
-              }
-              db.collection('locations').update({
-                _id: place_id
-              }, {
-                place: place_name,
-                temperature: forecastData.temp,
-                precipitation: forecastData.precipitation
-              })
+            db.collection('locations').update({
+              _id: place_id
+            }, {
+              place: place_name,
+              temperature: forecastData.temp,
+              precipitation: forecastData.precipitation
             })
           })
         })
-     // },7000)
+      })
+      res.render('index', {success: `Updated Successfully`,Places:``})
     }) 
 })
 app.get('/rainy', (req, res) => {
   MongoClient.connect(connectURL,{useNewUrlParser:true},(error,client) => {
     if(error){
-      return console.log('error')
+      return res.send('Cannot connect to database')
     }
     const db=client.db(dbname)
     var toGo2=[]
     db.collection('locations').find().forEach((loc)=>{
-      if(loc.precipitation>0){
-          var x =toGo2.push(loc.place)
+      if(loc.precipitation>0.20){
+          toGo2.push(loc.place)
       }
   
     }).then(()=>{
-      console.log("Rainy places:")
-      if(toGo2.length==0){
-          return console.log("No such destinations")
-      }
-      var iterator = toGo2.values();
-      for (let elements of iterator) { 
-        console.log(elements);
-      }
+     // var iterator = toGo2.values();
+     // for (let elements of iterator) { 
+     //   console.log(elements);
+     // }
+     if(toGo2.length==0){
+      return res.render('index', {success: `Rainy places:`,Places:`No such destinations`})
+     }
     })
-    res.render('index', {success: `Record Inserted Successfully`,Places:`Here are the countries`})
+    res.render('index', {success: `Rainy places:`,Places:toGo2})
   })
 })
 
 app.get('/warm', (req, res) => {
   MongoClient.connect(connectURL, {useNewUrlParser:true},(error,client) => {
     if(error){
-      return console.log('error')
+      return res.send('Cannot connect to database')
     }
     const db=client.db(dbname)
     var toGo=[]
     //for warm places
     db.collection('locations').find().forEach((loc)=>{
-     if(loc.temperature>25){
-         var x =toGo.push(loc.place)
+     if(loc.temperature>35){
+        toGo.push(loc.place)
      }
- 
-   }).then(()=>{
-     console.log("Warm places:")
-     if(toGo.length==0){
-         return console.log("No such destinations")
-     }
-     var iterator = toGo.values();
-     for (let elements of iterator) { 
-       console.log(elements);
-     }
-   })
-   res.render('index', {success: `Record Inserted Successfully`,Places:`Here are the countries`})
+    }).then(()=>{
+      if(toGo.length==0){
+      return  res.render('index.hbs', {success: `Warm places:`,Places:`No such destinations`})  
+      }      
+    })
+    res.render('index', {success: `Warm places:`,Places:toGo})
   })
 })
 
 app.get('/cold', (req, res) => {
   MongoClient.connect(connectURL, {useNewUrlParser:true},(error,client) => {
     if(error){
-      return console.log('error')
+      return res.send('Cannot connect to database')
     }
     const db=client.db(dbname)
     //for cold places
     var toGo1=[]
     db.collection('locations').find().forEach((loc)=>{
       if(loc.temperature<18){
-          var x =toGo1.push(loc.place)
+          toGo1.push(loc.place)
       }
 
     }).then(()=>{
-      console.log("Cold places:")
       if(toGo1.length==0){
-          return console.log("No such destinations")
-      }
-      var iterator = toGo1.values();
-      for (let elements of iterator) { 
-        console.log(elements);
+        return res.render('index', {success: `Cold places:`,Places:'No such destinations'})
       }
     })
-    res.render('index', {success: `Record Inserted Successfully`,Places:`Here are the countries`})
+    res.render('index', {success: `Cold places:`,Places:toGo1})
   })
 })
 app.get('/', (req, res) => {
-    res.render('index', { layout:false,success: ``,Places:``});
+    res.render('index', { layout:false,success: ``,Places:``,error:``});
   });
 
 app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
--- src/utils/countries.js
+++ src/utils/countries.js
@@ -6,85 +6,85 @@ const forecast=require('./forecast')
 const connectURL='mongodb://127.0.0.1:27017'
 const dbname='safar-api'
  MongoClient.connect(connectURL,{useNewUrlParser:true},(error,client)=>{
-  if(error){
+   if(error){
        return console.log('error')
-     }
-   const db=client.db(dbname)
-   var toGo=[]
-   //for warm places
-   db.collection('locations').find().forEach((loc)=>{
-    if(loc.temperature>25){
-        var x =toGo.push(loc.place)
-    }
+      }
+    const db=client.db(dbname)
+  //  var toGo=[]
+  //  //for warm places
+  //  db.collection('locations').find().forEach((loc)=>{
+  //   if(loc.temperature>25){
+  //       var x =toGo.push(loc.place)
+  //   }
 
-  }).then(()=>{
-    console.log("Warm places:")
-    if(toGo.length==0){
-        return console.log("No such destinations")
-    }
-    var iterator = toGo.values();
-    for (let elements of iterator) { 
-      console.log(elements);
-    }
-  })
-  //for cold places
-  var toGo1=[]
-  db.collection('locations').find().forEach((loc)=>{
-    if(loc.temperature<18){
-        var x =toGo1.push(loc.place)
-    }
+  // }).then(()=>{
+  //   console.log("Warm places:")
+  //   if(toGo.length==0){
+  //       return console.log("No such destinations")
+  //   }
+  //   var iterator = toGo.values();
+  //   for (let elements of iterator) { 
+  //     console.log(elements);
+  //   }
+  // })
+  // //for cold places
+  // var toGo1=[]
+  // db.collection('locations').find().forEach((loc)=>{
+  //   if(loc.temperature<18){
+  //       var x =toGo1.push(loc.place)
+  //   }
 
-  }).then(()=>{
-    console.log("Cold places:")
-    if(toGo1.length==0){
-        return console.log("No such destinations")
-    }
-    var iterator = toGo1.values();
-    for (let elements of iterator) { 
-      console.log(elements);
-    }
-  })
-  //rainy places
-  var toGo2=[]
-  db.collection('locations').find().forEach((loc)=>{
-    if(loc.precipitation>0){
-        var x =toGo2.push(loc.place)
-    }
+  // }).then(()=>{
+  //   console.log("Cold places:")
+  //   if(toGo1.length==0){
+  //       return console.log("No such destinations")
+  //   }
+  //   var iterator = toGo1.values();
+  //   for (let elements of iterator) { 
+  //     console.log(elements);
+  //   }
+  // })
+  // //rainy places
+  // var toGo2=[]
+  // db.collection('locations').find().forEach((loc)=>{
+  //   if(loc.precipitation>0){
+  //       var x =toGo2.push(loc.place)
+  //   }
 
-  }).then(()=>{
-    console.log("Rainy places:")
-    if(toGo2.length==0){
-        return console.log("No such destinations")
-    }
-    var iterator = toGo2.values();
-    for (let elements of iterator) { 
-      console.log(elements);
-    }
-  })
+  // }).then(()=>{
+  //   console.log("Rainy places:")
+  //   if(toGo2.length==0){
+  //       return console.log("No such destinations")
+  //   }
+  //   var iterator = toGo2.values();
+  //   for (let elements of iterator) { 
+  //     console.log(elements);
+  //   }
+  // })
 
 
-//     db.collection('locations').find().forEach(function(loc){
-//       var place_id = loc._id
-//       var place_name = loc.place
-//       geocode(loc.place,(error,{loc,latitude,longitude}={})=>{ 
-//         if(error){
-//           return console.log(error)
-//         }
+    db.collection('locations').find().forEach(function(loc){
+      var place_id = loc._id
+      var place_name = loc.place
+      geocode(loc.place,(error,{loc,latitude,longitude}={})=>{ 
+        if(error){
+          return console.log(error)
+        }
         
-//         forecast(latitude,longitude,(error,forecastData)=>{
-//           if(error){
-//             return console.log(error)
-//           }
-//           db.collection('locations').update({
-//             _id: place_id
-//           }, {
-//             place: place_name,
-//             temperature: forecastData.temp,
-//             precipitation: forecastData.precipitation
-//           })
-//         })
-//       })
-//     })
+        forecast(latitude,longitude,(error,forecastData)=>{
+          if(error){
+            return console.log(error)
+          }
+          db.collection('locations').update({
+            _id: place_id
+          }, {
+            place: place_name,
+            temperature: forecastData.temp,
+            precipitation: forecastData.precipitation
+          })
+        })
+      })
+    })
 
 
  })
